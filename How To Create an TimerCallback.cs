a2722fa0221414a772e0ddd3540d53f89b094341
Denis Rafi <62852838+DenisRafi@users.noreply.github.com>
2020-05-15 00:57:09 +0100

    Add files via upload

---
+++ How To Create an TimerCallback.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace CA
+{
+    class Program
+    {
+        static void Main()
+        {
+            Timer t = new Timer(TimerCallback, null, 0, 1000);         
+            Console.ReadLine();
+        }
+        private static void TimerCallback(Object o)
+        {
+            Console.WriteLine("In TimerCallback: " + DateTime.Now);
+            GC.Collect();
+        }
+    }
+}
+
